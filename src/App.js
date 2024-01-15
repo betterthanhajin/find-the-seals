d51f6471d175940a4a5499a5743e304c2f73a926
Hajin Lee <betterthanhajin@icloud.com>
2024-01-15 15:34:04 +0900

    feat: 초기 환경 설정

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import forest from "./spring-forest.png";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={forest} alt="background" />
+    </div>
+  );
+}
+
+export default App;
