e56e08ffb89cc4f54b55c3a5dbaf324c5eddc9ba
Hajin Lee <betterthanhajin@icloud.com>
2024-01-17 17:42:58 +0900

    feat: 물개 오브젝트 랜덤으로 위치

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import forest from "./spring-forest.png";
+import seal from "./seal.svg";
+import React, { useState, useEffect } from "react";
 
 import "./App.css";
 
-function App() {
+const RandomImages = ({ imageSrc, count }) => {
+  const [positions, setPositions] = useState([]);
+  useEffect(() => {
+    const generatePositions = () => {
+      const newPositions = [];
+      for (let i = 0; i < count; i++) {
+        const x = Math.random() * (window.innerWidth - 100);
+        const y = Math.random() * (window.innerHeight - 100);
+        newPositions.push({ x, y });
+      }
+      return newPositions;
+    };
+
+    setPositions(generatePositions());
+  }, [count]);
   return (
-    <div className="App">
-      <img src={forest} alt="background" />
+    <>
+      <div>
+        {positions.map((position, index) => (
+          <img
+            key={index}
+            src={imageSrc}
+            alt="seal"
+            style={{
+              width: "100px",
+              height: "100px",
+              position: "absolute",
+              left: `${position.x}px`,
+              top: `${position.y}px`,
+            }}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+export default function App() {
+  const imgageSrc = seal;
+  return (
+    <div>
+      <img
+        src={forest}
+        alt="forest"
+        style={{ width: "100%", height: "auto" }}
+      ></img>
+      <RandomImages imageSrc={imgageSrc} count={10} />;
     </div>
   );
 }
-
-export default App;
